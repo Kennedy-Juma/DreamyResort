0e6a1d1197d478696238cc91b0ae0b2a3d689ac2
Kennedy.Juma <kennedy.juma@sgnewtech.com>
2023-11-26 14:54:18 +0300

    Add project files.

---
+++ DreamyResort.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DreamyResort.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
